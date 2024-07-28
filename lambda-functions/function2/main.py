c12fbd23eb3ea9e7ab77cd91e910d4e0ca197af6
jodi.sihombing <jodi@DEV-LTP-763>
2024-07-28 23:51:19 +0700

    init feature/development

--- lambda-functions/function2/main.py
+++ lambda-functions/function2/main.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Function 2')
+        'body': json.dumps('Hello from Function 2 on development branch')
     }
