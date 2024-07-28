a23c57ac3800be732fb3fc9ceba026d49eec1bb8
jodi.sihombing <jodi@DEV-LTP-763>
2024-07-28 21:16:29 +0700

    init

--- lambda-functions/function2/main.py
+++ lambda-functions/function2/main.py
@@ -1,7 +1,18 @@
 import json
 
 def lambda_handler(event, context):
+    """
+    Lambda function handler.
+
+    Parameters:
+    event (dict): The event data.
+    context (object): The context object.
+
+    Returns:
+    dict: The response object.
+    """
+    # Currently, event and context are not used.
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Function 1')
+        'body': json.dumps('Hello from Function 2')
     }
